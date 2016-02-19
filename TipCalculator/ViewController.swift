f9825cfd3366fd2b11919932caa60a3fab97f3e8
Liem Ly Quan <liem.ly@cangroup.io>
2016-02-19 22:34:59 +0700

    Initi with basic function and UI based on tutorial

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var billField: UITextField!
+  @IBOutlet weak var tipRateLabel: UILabel!
+  @IBOutlet weak var totalLabel: UILabel!
+  
+  var tipRate:Int = 0
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    totalLabel.text = "$0.00"
+    tipRateLabel.text = "0%"
+
+    
+  }
+  @IBAction func onTap(sender: AnyObject) {
+    view.endEditing(true)
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func onEditingChange(sender: AnyObject) {
+    calculate()
+  }
 
+  @IBAction func onDrag(sender: UIPanGestureRecognizer) {
+    let translation = sender.translationInView(self.view!)
+    changeRate(Int(translation.x / 20))
+    calculate()
+  }
+  
+  func changeRate(rate:Int){
+    tipRate += rate
+    if (tipRate < 0){
+      tipRate = 0
+    } else if (tipRate > 100){
+      tipRate = 100
+    }
+    tipRateLabel.text = "\(tipRate)%"
+  }
 
+  func calculate(){
+    let billAmount = NSString(string: billField.text!).doubleValue
+    let tip = billAmount * Double(tipRate) / 100
+    let total = billAmount + tip
+    totalLabel.text = String(format: "$%.2f", total)
+  }
+  
 }
 
